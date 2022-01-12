bde86690005511c43261aff41ca6862fc67f7ed9
0sansNom <0sansnom2000@gmail.com>
2022-01-12 01:20:15 +0100

    rise performance

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Song = ({ currentSong }) => {
   return (
     <div className="song-container">
-      <img alt={currentSong.name} src={currentSong.cover} />
+      <img alt={currentSong.name} src={currentSong.cover} width="1024" height="1024" />
       <h2>{currentSong.name}</h2>
       <h3>{currentSong.artist}</h3>
     </div>
